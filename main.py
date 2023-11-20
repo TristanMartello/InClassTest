2bac2cda3c55e419e77506c2d8ebb72607d5fabd
TristanMartello <64500848+TristanMartello@users.noreply.github.com>
2023-11-20 09:57:14 -0500

    Combine the two main functions

--- main.py
+++ main.py
@@ -1,32 +1,35 @@
 #main.py
-from secrets import ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY, ssid, wifi_password
+from secrets import ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY
 import time
+import wifiLib
 import simple as mqtt
 
-def sending():
-    broker = '10.243.51.89' #change this as needed
-    ssid = 'Tufts_Wireless'
-    password = ''
-    topic_pub = "JordanandShiv" #publishing to this topic 
-    #topic_sub = "Pico/listen" #in case 2-way communication, subscribe to the topic that the other device is publishing to
-    
-    wlan = network.WLAN(network.STA_IF)
-    wlan.active(True)
-    wlan.connect(ssid, password)
-    while wlan.isconnected() == False:
-        print('Waiting for connection...')
-        time.sleep(1)
-    print("Connected to WiFi")
-    #print(wlan.ifconfig()) #this is supposed to get my IP Address...doesn't work idk
+host_ip = "0.0.0.0"
+host_channel = "haha"
+listen_channel = "whoa"
 
-    def whenCalled(topic, msg):
-        print((topic.decode(), msg.decode()))
+sending = ""
 
-    fred = mqtt.MQTTClient('BoneSend', broker, keepalive=600)
-    fred.connect()
+def sending(topic, msg):
+    global sending
+    sending = (msg.decode() + " " + time())
     
-    sending = (fred.check_msg() + " " + time())
+def main():
+    wifiLib.connect_wifi("tufts")
+    try:
+        broker = mqtt.MQTTClient('CarPico', host_ip, keepalive=6000)
+        print('Connected')
+        broker.connect()
+        broker.set_callback(sending)
+    except OSError as e:
+        print('Failed')
+        return
     
-    fred.publish(topic_pub, sending)
-        print(sending)
-        time.sleep(0.01)
+    # Subscribe to mqtt channels
+    broker.subscribe(host_channel)
+    while True:
+        if sending != "":
+            broker.publish(listen_channel, sending)
+    
+main()
+
